117321151c9217022a590f5c8d362d3f213cac70
Katerina Skroumpelou <sk.katherine@gmail.com>
2019-12-18 16:53:34 +0200

    some new things

--- src/app/services/notification.service.ts
+++ src/app/services/notification.service.ts
@@ -22,12 +22,15 @@ export class NotificationsService {
   }
 
   setNotification(message: string, action?: string, error?: any) {
-    console.log(error.code);
+    if (error) {
+      console.log(error.code);
+    }
+
     let notif_obj: Notification = {
       message: message,
       action: action ? action : "OK"
     };
-    if (error.code && error.code === "permission-denied") {
+    if (error && error.code && error.code === "permission-denied") {
       notif_obj.message = "You don't have permission to perform this action!";
     }
     this.notificationSubject.next(notif_obj);

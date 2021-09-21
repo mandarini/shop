98258e2831623ab9898511735421f45f86d15666
Katerina Skroumpelou <mandarini@users.noreply.github.com>
2021-09-21 20:07:22 +0300

    Removed API key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyDXSVe8FJGPHaYqY1bqq3_NotUGlTm5D-Y",
+    apiKey: "",
     authDomain: "shop-katers.firebaseapp.com",
     projectId: "shop-katers",
     storageBucket: "shop-katers.appspot.com",
-    messagingSenderId: "2792165170",
-    appId: "1:2792165170:web:1a92d6c30c68cc2b05912b",
-    measurementId: "G-WGSTVVHB16",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: "",
   },
 };
